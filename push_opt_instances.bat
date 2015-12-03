5f30f86bf70e8378ea9d370cc4e2549cf5b4f109
Pablo Pazos <pablo.pazos@cabolabs.com>
2015-12-03 01:22:24 -0300

    scripts

--- push_opt_instances.bat
+++ push_opt_instances.bat
@@ -1,1 +1,1 @@
-H:\putty\pscp -i "C:\Documents and Settings\pab\.ssh\default.ppk" "C:\Documents and Settings\pab\My Documents\GitHub\EhrCommitter\sample_instances\*.xml" 555167ba5973ca0a3d000113@cabolabs-ehrserver.rhcloud.com:jbossews/sample_instances
+H:\putty\pscp -i "C:\Documents and Settings\pab\.ssh\default.ppk" "C:\Documents and Settings\pab\My Documents\GitHub\EhrCommitter\sample_instances\*.xml" 56567cfa0c1e6617d20000b6@committer-ehrserver.rhcloud.com:jbossews/sample_instances
