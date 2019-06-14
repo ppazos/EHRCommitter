16947730b13e988e0cae610cda28aa895fb1c862
Pablo Pazos Gutierrez <pablo.pazos@cabolabs.com>
2019-06-14 00:59:43 -0300

    small fixes for keeping up with EHRServer updates

--- grails-app/controllers/com/cabolabs/CommitterController.groovy
+++ grails-app/controllers/com/cabolabs/CommitterController.groovy
@@ -9,7 +9,7 @@ import java.text.SimpleDateFormat
 class CommitterController {
 
    def config = Holders.config
-   
+
    def datetime_format_openEHR = "yyyyMMdd'T'HHmmss,SSSZ" // openEHR format
    def datetime_format_html5   = "yyyy-MM-dd HH:mm:ss" // HTML5 format
 
@@ -22,7 +22,7 @@ class CommitterController {
             flash.message = 'Please specify all the authentication values'
             return
          }
-         
+
          // service login
          // set token on session
          def ehr = new RESTClient(config.server.protocol + config.server.ip +':'+ config.server.port + config.server.path)
@@ -34,12 +34,12 @@ class CommitterController {
                requestContentType: URLENC,
                body: [username: username, password: password, organization: orgnumber]
             )
-            
+
             //println "res: " + res.responseData
             //println "res: " + res.responseData.token
-            
+
             session.token = res.responseData.token
-            
+
             redirect action:'index'
             return
          }
@@ -51,72 +51,73 @@ class CommitterController {
          }
       }
    }
-   
+
    def logout()
    {
       session.token = null
-      
+
       redirect action:'login'
       return
    }
-    
+
    def index()
    {
        def path = Holders.config.ehr.instance_repo
-       
+
        println path
-       
+
        def repo = new File( path )
-       
+
        println repo
-       
+
        def files = []
        repo.eachFileMatch groovy.io.FileType.FILES, ~/.*\.xml/, { file ->
-         
+
          //println "indexAll file: "+ file.name
          files << file.name - ".xml"
        }
-       
+
        println files
-       
+
        [files: files]
    }
-    
+
    def create(String template)
    {
    println template
        def filename = template
        def path = config.ehr.instance_repo
        def file = new File(path + filename + ".xml")
-       
+
        def xml = file.text
+       println xml
        def tags = xml.findAll(/\[\[.*:::.*:::.*\]\]/)
-       
+
        [file: file, tags: tags, ehrs: this.ehrs()]
    }
-    
+
    private List ehrs()
    {
       def ehrs = []
-      
+
       log.info( "Consulta al EHR: "+ config.server.protocol + config.server.ip +':'+ config.server.port + config.server.path +'api/v1/ehrs')
-      
+
       def http = new HTTPBuilder(config.server.protocol + config.server.ip +':'+ config.server.port + config.server.path +'api/v1/ehrs')
-      
+
       // Si no hay conexion con el servidor tira excepcion
       try
       {
          // perform a GET request, expecting TEXT response data
          http.request( Method.GET, ContentType.JSON ) { req ->
-           
+
            uri.query = [ format: 'json' ]
-         
+
            headers.'User-Agent' = 'Mozilla/5.0 Ubuntu/8.10 Firefox/3.0.4'
            headers.'Authorization' = 'Bearer '+ session.token
-           
+
            req.getParams().setParameter("http.connection.timeout", new Integer(10000));
            req.getParams().setParameter("http.socket.timeout", new Integer(10000));
-         
+
            response.success = { resp, json ->
 
               //ehrs = json.ehrs
@@ -124,12 +125,12 @@ class CommitterController {
                 ehrs << ehr
               }
            }
-         
+
            // handler for any failure status code:
            response.failure = { resp ->
-              
+
               println "response: " + resp.getAllHeaders() + " curr thrd id: " + Thread.currentThread().getId()
-              
+
               //println "Unexpected error: ${resp.statusLine.statusCode} : ${resp.statusLine.reasonPhrase}"
               throw new Exception("Unexpected error: ${resp.statusLine.statusCode} : ${resp.statusLine.reasonPhrase}")
            }
@@ -143,51 +144,51 @@ class CommitterController {
       {
          log.error ( e.message )
       }
-      
+
       return ehrs
    }
-   
+
    def save(String filename, String ehr_uid, String COMMITTER_NAME)
    {
       println params
-      
+
       def path = config.ehr.instance_repo
       def file = new File(path + filename + ".xml")
       def xml = file.text
-      
+
       // Para las fechas vienen el date y el time separados
       def params2 = params.collectEntries
       {
         println it.key +' '+ it.value
-        
+
         if (it.value instanceof String[]) // el valor es date, time
         {
            println "DATE"
            //[(it.key): it.value[0]+" "+it.value[1]] // formato HTML5 "yyyy-MM-dd HH:mm:ss"
-           
+
            // Transform yyyy-MM-dd HH:mm:ss to yyyyMMddTHHmmss.sss
-           
+
            def date_text = it.value[0]+" "+it.value[1]
-           
+
            // Avoid empty values
            if (date_text == " ") [(it.key): ""]
            else
            {
               def format = new SimpleDateFormat(this.datetime_format_html5)
               def date = format.parse( date_text )
-              
+
               def format_oehr = new SimpleDateFormat(this.datetime_format_openEHR)
               def str_date_openEHR = format_oehr.format(date)
-              
+
               println "date: " + str_date_openEHR
-              
+
               [(it.key): str_date_openEHR]
            }
         }
         else if (it.key.startsWith("DVCT_")) // data comes from a coded text
         {
            println "CODED"
-          
+
            if (!it.value)
            {
               /*
@@ -213,7 +214,7 @@ class CommitterController {
               def (text, code, terminology_id) = it.value.split("::")
               /*
               TBD (it.value is text::code::terminology_id)
-             
+
               <value xsi:type="DV_CODED_TEXT">
                 <value>Leve</value>
                 <defining_code>
@@ -224,7 +225,7 @@ class CommitterController {
                 </defining_code>
               </value>
               */
-             
+
               [(it.key - "DVCT_"): $/<value xsi:type="DV_CODED_TEXT">
                 <value>${text}</value>
                 <defining_code>
@@ -254,7 +255,7 @@ class CommitterController {
               </symbol>
             </value>
           */
-          
+
           [(it.key - "DVORD_"): $/<value xsi:type="DV_ORDINAL">
             <value>${ordinal}</value>
             <symbol>
@@ -279,69 +280,69 @@ class CommitterController {
            [(it.key): it.value]
         }
       }
-      
+
       println params2
-      
+
       def escaped_k, escaped_v
       params2.each { k, v ->
          //println v.getClass() // String o Nullobject
-         
+
          // If k has parenthesis, the replacement is not done.
 
          escaped_k = k.replace('?', '\\?') // if k has a ?, it is not replaced
          escaped_k = escaped_k.replace('*', '\\*') // if k has a *, it is not replaced
          escaped_k = escaped_k.replace('&', '&amp;')
-         
+
          if (k != escaped_k)
          {
             println "replacing "+ k +" "+ escaped_k
          }
-         
+
          escaped_v = v.replace('&', '&amp;')
-         
+
          if (v != escaped_v)
          {
             println "replacing "+ v +" "+ escaped_v
          }
-         
+
          xml = xml.replaceAll( /\[\[${escaped_k}:::.*:::.*\]\]/, escaped_v)
 
          //xml.replaceAll(k, v) // FIXME: lo que hay que sustituir es esto [[PROBLEMA_RESOLUCION:::DATE:::EMPTY]] NO solo el nombre...
       }
-      
+
       //println xml
-      
-      
+
+
       // Write edited file to disk
       def random = new java.util.Random()
       def committed = new File(config.ehr.instance_repo + File.separator +'committed' + File.separator +'version_'+ random.nextInt(10000) +'.xml')
       committed << xml
-      
-      
+
+
       def res = commit(xml, ehr_uid, COMMITTER_NAME)
-      
+
       println res
-      
+
       redirect action:"index" // show commit result
    }
-   
+
    private String commit(String xml, String ehrUid, String committer_name) {
-   
+
       def ehr = new RESTClient(config.server.protocol + config.server.ip +':'+ config.server.port + config.server.path)
       def res
-      
+
       println "ehrUid: $ehrUid"
-      
+
       try
       {
          // remove XML declaration and internal namespaces
          xml = xml.replace('<?xml version="1.0" encoding="UTF-8"?>', '')
          xml = xml.replace('xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"', '')
          xml = xml.replace('xmlns="http://schemas.openehr.org/v1"', '')
-         
+
          // add namespace only on the root
          xml = '<versions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://schemas.openehr.org/v1">'+ xml +'</versions>'
-         
+
          // Sin URLENC da error null pointer exception sin mas datos... no se porque es. PREGUNTAR!
          res = ehr.post(
             path:"api/v1/ehrs/${ehrUid}/compositions",
@@ -364,8 +365,8 @@ class CommitterController {
               </details>
             </result>
           */
-         
-         
+
+
          println "------"
          if (res.status in 200..299)
          {
@@ -375,11 +376,11 @@ class CommitterController {
          {
             println "Status ERROR: "+ res.statusLine.statusCode +' '+ res.statusLine.reasonPhrase
          }
-         
+
          println "Result Type: "+ res.data.type.text()
          println "Result Message: "+ res.data.message.text()
          println "Result Code: "+ res.data.code.text()
-         
+
          if (!res.data.details.isEmpty())
          {
             println "Result Details:"
@@ -387,30 +388,30 @@ class CommitterController {
                println " - "+ it
             }
          }
-         
+
          // text/xml
          println "Result Content: "+ res.contentType
          println "------"
          println ""
-         
-         
+
+
          // Versions were committed successfully ...
          //println "res: " + res.responseData.message.text()
-         
+
          // EHR_SERVER::API::RESPONSE_CODES::1324
          //println res.data.code.text()
          //println res.data.getClass() // NodeChild
          //println "res2: " + res.responseData.getClass() // NodeChild
          //println "res3: " + res.responseData.name() // result
-         
+
          // 202 Accepted
          //println res.statusLine.statusCode +' '+ res.statusLine.reasonPhrase
          //println res.statusLine.statusCode.getClass() // Integer
-         
+
          // 202
          ///println res.status
          //println res.status.getClass() // Integer
-         
+
          return res.data.message.text() // res.responseData.message
       }
       catch (Exception e)
@@ -418,14 +419,14 @@ class CommitterController {
          println "except commit:" + e.message
          /*
          println e.response.data
-         
+
          e.printStackTrace(System.out)
-         
+
          //if (e?.response.status == 404)
          //{
             // TODO> ver que hacer con los errores
          //}
-         
+
          return e.message
          */
       }
--- grails-app/taglib/ehrcommitter/EhrformTagLib.groovy
+++ grails-app/taglib/ehrcommitter/EhrformTagLib.groovy
@@ -2,204 +2,256 @@ package ehrcommitter
 
 import java.util.UUID
 import java.text.SimpleDateFormat
+import java.text.DecimalFormat
 
 class EhrformTagLib {
-    //static defaultEncodeAs = [taglib:'html']
-    //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
-    
-    // Do not generate inputs for these names
-    def filter = ["PATIENT_UID"]
-    
-    def display = { attrs, body ->
-    
-       //println "attrs "+ attrs
-    
-       // [[name::type]]
-       // name::type
-       // name, type
-       def (name, type, defaultValue) = attrs.tag[2..-3].split(':::')
-       
-       println "'"+ name +" "+ type +" "+ defaultValue +"'"
-       
-       if (filter.contains(name)) return
-       
-       def method = "display"+type
-       out << this."$method"(name, defaultValue)
-    }
-    
-    def displayINTEGER(String name, String defaultValue) {
-    
-       if (defaultValue == "RANDOM")
-       {
-          defaultValue = intGenerator(1000, 0) // 0..1000
-       }
-       else if (defaultValue == "EMPTY")
-       {
-          defaultValue = 1
-       }
-       else if (defaultValue.startsWith('RANGE_')) // RANGE_100-150
-       {
-          //println 'parseInt '+ defaultValue +' '+ (defaultValue - "RANGE_").split('\\.\\.')
-          def range = (defaultValue - "RANGE_").split('\\.\\.').collect{ Integer.parseInt(it) } // escape to avoid spliting by each character 
-          defaultValue = intGenerator(range[1] - range[0], range[0]) // range[0]..range[1]
-       }
-    
-       """<label>${name}</label>
-       <input type="number" name="${name}" value="${defaultValue}" class="form-control" />
-       """
-    }
-    
-    def displaySTRING(String name, String defaultValue) {
-    
-       if (defaultValue == "RANDOM")
-       {
-          defaultValue = generator( (('A'..'Z')+('a'..'z')+' ').join(), 30 )
-       }
-       else if (defaultValue == "EMPTY")
-       {
-          defaultValue = ""
-       }
-    
-       $/<label>${name}</label>
-       <textarea name="${name}" class="form-control">${defaultValue}</textarea>
-       /$
-    }
-    
-    def displayCODEDTEXT(String name, String defaultValue) {
-    
-       // If the coded text has a terminologic constraint, the defaultValue will be empty "()" because there are no codes in the template.
-       
-       // (Interim::at0037::local, Final::at0038::local,,,Supplementary::at0039::local,,,Corrected::at0040::local,,,Aborted::ar0074::local,,,Never performed::at0079::local)
-       def list = []
-       if (defaultValue != "()")
-       {
-          list = defaultValue[1..-2].split(",,,").collect{ it.split("::") } // [[Interim, at0037, local], [ Final, at0038, local], [Supplementary, at0039, local], [Corrected, at0040, local], [Aborted, ar0074, local], [Never performed, at0079, local]]
-       }
-       
-       def options = '<option value=""></option>'
-       list.each { coded_text_triad ->
-         options += '<option value="'+ coded_text_triad[0] +'::'+ coded_text_triad[1] +'::'+ coded_text_triad[2] +'">'+ coded_text_triad[0] +'</option>'
-       }
-       
-       // with the prefix DVCT_ we know in the controller this data comes from a DV_CODED_TEXT
-       $/<label>${name}</label>
-       <select name="DVCT_${name}" class="form-control">
-         ${options}
-       </select>
-       /$
-    }
-    
-    /*
-     * like the coded text but with the integer ordinal.
-     * (text::atcode::terminologyid::ordinal, text::atcode::terminologyid::ordinal, text::atcode::terminologyid::ordinal, ...)
-     */
-    def displayORDINAL(String name, String defaultValue)
-    {
-       def list = []
-       if (defaultValue != "()")
-       {
-          println "ordinal parseInt "+ defaultValue
-          list = defaultValue[1..-2].split(",").collect{ it.split("::") } // [[text, atcode, terminologyid, ordinal], ... ]
-          list.each { it[3] = Integer.parseInt( it[3] ) } // ordinals to integers, can throw an exception if it is not a number, TODO: check
-          list.sort{ it[3] } // sort by ordinal
-       }
-       
-       def options = '<option value=""></option>'
-       list.each { ordinal ->
-         options += '<option value="'+ ordinal[0] +'::'+ ordinal[1] +'::'+ ordinal[2] +'::'+ ordinal[3] +'">'+ ordinal[0] +'</option>'
-       }
-       
-       // with the prefix DVORD_ we know in the controller this data comes from a DV_ORDINAL
-       $/<label>${name}</label>
-       <select name="DVORD_${name}" class="form-control">
-         ${options}
-       </select>
-       /$
-    }
-    
-    def displayDATE(String name, String defaultValue) {
-    
-       if (defaultValue == "NOW")
-       {
-          def now = new Date()
-          //def date_format = "yyyyMMdd" // openEHR format
-          def date_format = "yyyy-MM-dd" // HTML5 format 
-          def formatter = new SimpleDateFormat( date_format )
-          defaultValue = formatter.format( now )
-       }
-       else
-          defaultValue = ''
-          
-       """<label>${name}</label>
-       <input type="date" name="${name}" value="${defaultValue}" class="form-control" />
-       """
-    }
-    
-    def displayDATETIME(String name, String defaultValue) {
-    
-       def defaultValueDate, defaultValueTime
-    
-       if (defaultValue == "NOW")
-       {
-          def now = new Date()
-          //def datetime_format = "yyyyMMdd'T'HHmmss,SSSZ" // openEHR format
-          def datetime_format = "yyyy-MM-dd HH:mm:ss" // HTML5 format
-          def formatter = new SimpleDateFormat( datetime_format )
-          def datetime = formatter.format( now )
-          (defaultValueDate, defaultValueTime) = datetime.split(' ')
-       }
-       else
-       {
-          defaultValueDate = ''
-          defaultValueTime = ''
-       }
-    
-       """<label>${name}</label>
-       <input type="date" name="${name}" value="${defaultValueDate}" class="form-control" />
-       <input type="time" name="${name}" value="${defaultValueTime}" class="form-control" />
-       """
-    }
-    
-    def displayUUID(String name, String defaultValue) {
-    
-       if (defaultValue == "ANY")
-          defaultValue = UUID.randomUUID().toString()
-       
-       """<label>${name}</label>
-       <input type="text" name="${name}" value="${defaultValue}" class="form-control" />
-       """
-    }
-    
-    // Generates an OBJECT_VERSION_ID object_id ‘::’ creating_system_id ‘::’ version_tree_id
-    //[[VERSION:::UUID:::ANY]]::[[APP:::STRING:::EMR]]::1
-    def displayVERSION_ID(String name, String defaultValue) {
-    
-       if (defaultValue == "ANY")
-          defaultValue = UUID.randomUUID().toString() +"::EMR::1"
-       
-       """<label>${name}</label>
-       <input type="text" name="${name}" value="${defaultValue}" class="form-control" />
-       """
-    }
-    
-    def displayDURATION(String name, String defaultValue) {
-       
-       if (defaultValue == "ANY")
-          defaultValue = "PT30M"
-       
-       $/<label>${name}</label>
-       <input type="text" class="form-control" name="${name}" value="${defaultValue}" pattern="P(?=\w*\d)(?:\d+Y|Y)?(?:\d+M|M)?(?:\d+W|W)?(?:\d+D|D)?(?:T(?:\d+H|H)?(?:\d+M|M)?(?:\d+(?:\­.\d{1,2})?S|S)?)?" />
-       /$
-    }
-    
-    // generator( (('A'..'Z')+('0'..'9')).join(), 9 )
-    def generator = { String alphabet, int n ->
-       new Random().with {
-         (1..n).collect { alphabet[ nextInt( alphabet.length() ) ] }.join()
-       }
-    }
-    
-    def intGenerator = { n, base ->
-       new Random( System.currentTimeMillis() ).nextInt( n+1 ) + base
-    }
+   //static defaultEncodeAs = [taglib:'html']
+   //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
+
+   // Do not generate inputs for these names
+   def filter = ["PATIENT_UID"]
+
+   def display = { attrs, body ->
+
+      //println "attrs "+ attrs.tag[2..-3].split(':::')
+
+      // [[name::type]]
+      // name::type
+      // name, type
+      def (name, type, defaultValue) = attrs.tag[2..-3].split(':::')
+
+      //println "'"+ name +" "+ type +" "+ defaultValue +"'"
+
+      if (filter.contains(name)) return
+
+      def method = "display"+type
+      out << this."$method"(name, defaultValue)
+   }
+
+   def displayINTEGER(String name, String defaultValue) {
+
+      if (defaultValue == "RANDOM")
+      {
+         defaultValue = intGenerator(1000, 0) // 0..1000
+      }
+      else if (defaultValue == "EMPTY")
+      {
+         defaultValue = 1
+      }
+      else if (defaultValue.startsWith('RANGE_')) // RANGE_100-150
+      {
+         //println 'parseInt '+ defaultValue +' '+ (defaultValue - "RANGE_").split('\\.\\.')
+         def range = (defaultValue - "RANGE_").split('\\.\\.').collect{ Integer.parseInt(it) } // escape to avoid spliting by each character
+         defaultValue = intGenerator(range[1] - range[0], range[0]) // range[0]..range[1]
+      }
+
+      """<label>${name}</label>
+      <input type="number" name="${name}" value="${defaultValue}" class="form-control" />
+      """
+   }
+
+   def displayREAL(String name, String defaultValue) {
+
+      def value
+      if (defaultValue == "RANDOM")
+      {
+         value = intGenerator(1000, 0) // 0..1000
+      }
+      else if (defaultValue == "EMPTY")
+      {
+         value = 1.0
+      }
+      else if (defaultValue.startsWith('RANGE_')) // RANGE_100.0-150.0
+      {
+         def range = (defaultValue - "RANGE_").split('\\.\\.').collect{ Float.parseFloat(it) } // escape to avoid spliting by each character
+         value = floatGenerator(range[1] - range[0], range[0]) // range[0]..range[1]
+      }
+
+      DecimalFormat df = new DecimalFormat("#.#");
+      String formattedValue = df.format(value);
+
+
+      """<label>${name}</label>
+      <input type="number" name="${name}" value="${formattedValue}" class="form-control" step="0.1" />
+      """
+   }
+
+   def displayLIST(String name, String defaultValue) {
+
+      // [a, b, c]
+      def list = []
+      if (defaultValue != "[]")
+      {
+        list = defaultValue[1..-2].split(", ")
+      }
+
+      def options = '<option value=""></option>'
+      list.each { item ->
+       options += '<option value="'+ item +'">'+ item +'</option>'
+      }
+
+      $/<label>${name}</label>
+      <select name="${name}" class="form-control">
+       ${options}
+      </select>
+      /$
+   }
+
+   def displaySTRING(String name, String defaultValue) {
+
+      if (defaultValue == "RANDOM")
+      {
+         defaultValue = generator( (('A'..'Z')+('a'..'z')+' ').join(), 30 )
+      }
+      else if (defaultValue == "EMPTY")
+      {
+         defaultValue = ""
+      }
+
+      $/<label>${name}</label>
+      <textarea name="${name}" class="form-control">${defaultValue}</textarea>
+      /$
+   }
+
+   def displayCODEDTEXT(String name, String defaultValue) {
+
+      // If the coded text has a terminologic constraint, the defaultValue will be empty "()" because there are no codes in the template.
+
+      // (Interim::at0037::local, Final::at0038::local,,,Supplementary::at0039::local,,,Corrected::at0040::local,,,Aborted::ar0074::local,,,Never performed::at0079::local)
+      def list = []
+      if (defaultValue != "()")
+      {
+        list = defaultValue[1..-2].split(",,,").collect{ it.split("::") } // [[Interim, at0037, local], [ Final, at0038, local], [Supplementary, at0039, local], [Corrected, at0040, local], [Aborted, ar0074, local], [Never performed, at0079, local]]
+      }
+
+      def options = '<option value=""></option>'
+      list.each { coded_text_triad ->
+       options += '<option value="'+ coded_text_triad[0] +'::'+ coded_text_triad[1] +'::'+ coded_text_triad[2] +'">'+ coded_text_triad[0] +'</option>'
+      }
+
+      // with the prefix DVCT_ we know in the controller this data comes from a DV_CODED_TEXT
+      $/<label>${name}</label>
+      <select name="DVCT_${name}" class="form-control">
+       ${options}
+      </select>
+      /$
+   }
+
+   /*
+    * like the coded text but with the integer ordinal.
+    * (text::atcode::terminologyid::ordinal, text::atcode::terminologyid::ordinal, text::atcode::terminologyid::ordinal, ...)
+    */
+   def displayORDINAL(String name, String defaultValue)
+   {
+      def list = []
+      if (defaultValue != "()")
+      {
+        println "ordinal parseInt "+ defaultValue
+        list = defaultValue[1..-2].split(",").collect{ it.split("::") } // [[text, atcode, terminologyid, ordinal], ... ]
+        list.each { it[3] = Integer.parseInt( it[3] ) } // ordinals to integers, can throw an exception if it is not a number, TODO: check
+        list.sort{ it[3] } // sort by ordinal
+      }
+
+      def options = '<option value=""></option>'
+      list.each { ordinal ->
+       options += '<option value="'+ ordinal[0] +'::'+ ordinal[1] +'::'+ ordinal[2] +'::'+ ordinal[3] +'">'+ ordinal[0] +'</option>'
+      }
+
+      // with the prefix DVORD_ we know in the controller this data comes from a DV_ORDINAL
+      $/<label>${name}</label>
+      <select name="DVORD_${name}" class="form-control">
+       ${options}
+      </select>
+      /$
+   }
+
+   def displayDATE(String name, String defaultValue) {
+
+      if (defaultValue == "NOW")
+      {
+        def now = new Date()
+        //def date_format = "yyyyMMdd" // openEHR format
+        def date_format = "yyyy-MM-dd" // HTML5 format
+        def formatter = new SimpleDateFormat( date_format )
+        defaultValue = formatter.format( now )
+      }
+      else
+        defaultValue = ''
+
+      """<label>${name}</label>
+      <input type="date" name="${name}" value="${defaultValue}" class="form-control" />
+      """
+   }
+
+   def displayDATETIME(String name, String defaultValue) {
+
+      def defaultValueDate, defaultValueTime
+
+      if (defaultValue == "NOW")
+      {
+        def now = new Date()
+        //def datetime_format = "yyyyMMdd'T'HHmmss,SSSZ" // openEHR format
+        def datetime_format = "yyyy-MM-dd HH:mm:ss" // HTML5 format
+        def formatter = new SimpleDateFormat( datetime_format )
+        def datetime = formatter.format( now )
+        (defaultValueDate, defaultValueTime) = datetime.split(' ')
+      }
+      else
+      {
+        defaultValueDate = ''
+        defaultValueTime = ''
+      }
+
+      """<label>${name}</label>
+      <input type="date" name="${name}" value="${defaultValueDate}" class="form-control" />
+      <input type="time" name="${name}" value="${defaultValueTime}" class="form-control" />
+      """
+   }
+
+   def displayUUID(String name, String defaultValue) {
+
+      if (defaultValue == "ANY")
+        defaultValue = UUID.randomUUID().toString()
+
+      """<label>${name}</label>
+      <input type="text" name="${name}" value="${defaultValue}" class="form-control" />
+      """
+   }
+
+   // Generates an OBJECT_VERSION_ID object_id ‘::’ creating_system_id ‘::’ version_tree_id
+   //[[VERSION:::UUID:::ANY]]::[[APP:::STRING:::EMR]]::1
+   def displayVERSION_ID(String name, String defaultValue) {
+
+      if (defaultValue == "ANY")
+        defaultValue = UUID.randomUUID().toString() +"::EMR::1"
+
+      """<label>${name}</label>
+      <input type="text" name="${name}" value="${defaultValue}" class="form-control" />
+      """
+   }
+
+   def displayDURATION(String name, String defaultValue) {
+
+      if (defaultValue == "ANY")
+        defaultValue = "PT30M"
+
+      $/<label>${name}</label>
+      <input type="text" class="form-control" name="${name}" value="${defaultValue}" pattern="P(?=\w*\d)(?:\d+Y|Y)?(?:\d+M|M)?(?:\d+W|W)?(?:\d+D|D)?(?:T(?:\d+H|H)?(?:\d+M|M)?(?:\d+(?:\­.\d{1,2})?S|S)?)?" />
+      /$
+   }
+
+   // generator( (('A'..'Z')+('0'..'9')).join(), 9 )
+   def generator = { String alphabet, int n ->
+      new Random().with {
+       (1..n).collect { alphabet[ nextInt( alphabet.length() ) ] }.join()
+      }
+   }
+
+   def intGenerator = { n, base ->
+      new Random( System.currentTimeMillis() ).nextInt( n+1 ) + base
+   }
+
+   def floatGenerator = { range, base ->
+      (Math.random() * range) + base
+   }
 }
